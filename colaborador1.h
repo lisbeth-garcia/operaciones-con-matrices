84bda5cd3ef0c45e369ccc5f7d3396cfde17620e
lisbeth-garcia <86217075+lisbeth-garcia@users.noreply.github.com>
2021-07-03 17:37:55 -0500

    Update colaborador1.h
    garcia lopez lisbeth colaborador1.h   declaramos nuestras variables  flag que se utiliza para darle una señal al programa que sepa que ha cumplido una determinada condicion, se utilizo la variable void que su funcion fue para un tipo de valor devuelto de funciom, es decir, no devuelve ningun valor que el programa este ejecutando.

--- colaborador1.h
+++ colaborador1.h
@@ -1,47 +1,53 @@
+//garcia lopez lisbeth 
 #include<iostream>
 #include<stdlib.h>
 
 using namespace std;
 
 int main()
-{
-	int op,flag;
-	float matriz1[5][5],matriz2[5][5],resultado[5][5]={{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
 	
 
+{
+	int op,flag;
+	
+	float matriz1[5][5],matriz2[5][5],resultado[5][5]={{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
+	
     for(int i=0;i<5;i++){
     	for(int j=0;j<5;j++) {
     	cout<<"Digite su matriz1 ["<<i<<"]["<<j<<"]: ";
     	cin>>matriz1[i][j];
 	}
-    cout<<"\n";
+	 
+	void mostrar (float (*r)[5]);
 }
 
-void ingresar(float (*m1)[5], float (*m2)[5]);
-
-{
+    cout<<"\nmatriz 1\n";
+    
+    for (int i=0;i<5;i++){
+	    for(int j=0;j<5;j++) {
+		cout<<matriz1[i][j]<<" ";
+	}
 	
-cout<<"Ingresar y mostrar los valores de los arreglos o matrices";
-for(int i=0;i<5;i++){
-    	for(int j=0;j<5;j++) 
-		{
+	cout<<"\n";
+}
+
+
+    for(int i=0;i<5;i++){
+    	for(int j=0;j<5;j++) {
     	cout<<"Digite su matriz2 ["<<i<<"]["<<j<<"]: ";
     	cin>>matriz2[i][j];
 	}
-}}
+}
 
-cout<<"\nmatriz 2\n";
-for (int i=0;i<5;i++){
-	for(int j=0;j<5;j++) 
-	{
+    cout<<"\nmatriz 2\n";
+        for (int i=0;i<5;i++){
+	    for(int j=0;j<5;j++) {
 		cout<<matriz2[i][j]<<" ";
 	}
+	
 	cout<<"\n";
 }
 
 return (0);
 
 }
-
-
-
