6bf6eda667137f8292d7e4b2dc93e07275d6feb1
DianaEspinal06 <86257265+DianaEspinal06@users.noreply.github.com>
2021-07-03 18:32:36 -0500

    DIANA ESPINAL
    A MI ME TOCO SACAR LA POTENCIA DE LA MATRIZ

--- colaborador3.h
+++ colaborador3.h
@@ -1,5 +1,26 @@
 void potencia(float (*m1)[5], int p, float (*r)[5])
 {
+  break;
+		case 3:
+		cout<<"Calcular la potencia \n";
+		cout<< "El resultado de la matriz1 elevada a 2 es:\n";
+		for(i=0;i<3;i++){
+			for (j=0;j<3;j++){
+				resultado[i][j]=0;
+				for(k=0;k<3;k++){
+					resultado[i][j]=resultado[i][j]+(matriz1[i][k]*matriz1[k][j]);
+				}
+				
+			}
+		}
+
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			cout<<resultado[i][j]<<" ";
+			cout<<"\n";
+		}
+	}
+  
 
 cout<<"Calcular la potencia p de una ";
 
