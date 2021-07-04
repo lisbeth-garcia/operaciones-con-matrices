9f06c18922242cd606e8bd1e1148d4e284ac84e7
Fernandocajiao <86032081+Fernandocajiao@users.noreply.github.com>
2021-07-03 19:52:49 -0500

    Update colaborador4.h
    A mi me toco la transpuesta hay esta lo que me corresponde a mi

--- colaborador4.h
+++ colaborador4.h
@@ -1,9 +1,22 @@
+//Fernando Cajiao Casierra
 void transpuesta(float (*m1)[5], float (*r)[5])
 {
 
 cout<<"Obtener la transpuesta de una matriz";
-
-
-
-
+cout<<"\nElementos de la matriz1 original : \n";
+for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+	      cout<<m1[i][j]<<" ";
+	}
+cout<<"\n";
+	}
+cout<<endl<<endl;
+cout<<"\nElementos de la matriz1 Transpuesta : \n";
+for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+		r[j][i]=m1[j][i];
+	      cout<<r[j][i]<<" ";
+	}
+cout<<"\n";
+	}
 }
