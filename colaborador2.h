131bfb94a06804e95a7aa888fb639b21a5954553
Pierina Mejía <86214289+Mejia12@users.noreply.github.com>
2021-07-03 20:58:55 -0500

    Update colaborador2.h
    Yo hice la parte de obtener el producto de las dos matrices con cada uno de los elementos de la matriz1 y la matriz2, el proceso es el siguiente dónde utilizaré un ciclo o bucle (para "for") para resolver este ejercicio.

--- colaborador2.h
+++ colaborador2.h
@@ -1,9 +1,44 @@
-void producto(float (*m1)[5], float (*m2)[5], float (*r)[5])
+// Pierina Mejía Hurtado
+
+void producto(float (*m1)[5], float (*m2)[5],float (*r)[5])
 {
+int filas=5,columnas=5;
+cout<<"PRODUCTO DE LAS MATRICES\n\n";
 
-cout<<"Obetener el producto de las dos matrices ";
-
-
+	for(int i=0;i<filas;i++){
+		for(int j=0;j<columnas;j++){
+		for(int k=0;k<columnas;k++){
+				r[i][j]+=m1[i][k]*m2[k][j];
+			
+		
+		}
+	} 
+}
+		cout<<"\nMostrando matriz #1\n\n";
+	
+	for(int i=0;i<filas;i++){
+		for(int j=0;j<columnas;j++){
+			cout<<m1[j][i]<<"  ";
+		}
+		cout<<"\n\n";
+	}
+	cout<<"\nMostrando matriz #2\n\n";
+	
+	for(int i=0;i<filas;i++){
+		for(int j=0;j<columnas;j++){
+			cout<<m2[j][i]<<"  ";
+		}
+		cout<<"\n\n";
+	}
+	cout<<"El producto de las dos matrices es:\n";
+	
+	for(int i=0;i<filas;i++){
+		for(int j=0;j<columnas;j++){
+			
+			cout<<r[j][i]<<"  ";
+		}
+		cout<<"\n\n";
+	}
 
 
 }
