fd9ae726619c1e7fd77918df0cade811df0b878f
DianaEspinal06 <86257265+DianaEspinal06@users.noreply.github.com>
2021-07-03 19:04:12 -0500

    Diana Espinal Marin
    yo Diana Espinal me toco la parte de la potencia lo cual hice los siguientes procesos para avanzar con lo que me toco

--- colaborador3.h
+++ colaborador3.h
@@ -1,8 +1,23 @@
+//Diana Espinal Marin
 void potencia(float (*m1)[5], int p, float (*r)[5])
 {
+  cout<<"\nElementos de la matriz1 original : \n";
+for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+	      cout<<m1[i][j]<<" ";
+	}
+cout<<"\n";
+	}
 
 cout<<"Calcular la potencia p de una ";
-
+for(int i=0; i<5; ++i)
+        for(int j=0; j<5; ++j)
+            r[i][j] = 0;
+            
+            for(int i=0; i<5; ++i)
+        for(int j=0; j<5; ++j)
+            for(int z=0; z<5; ++z)
+                r[i][j] += m1[i][z] * m1[z][j];
 
 
 
