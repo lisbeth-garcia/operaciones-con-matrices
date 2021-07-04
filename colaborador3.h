cf16be7cd79113a1e312caa17de9b31494b50cf4
DianaEspinal06 <86257265+DianaEspinal06@users.noreply.github.com>
2021-07-03 21:24:52 -0500

    Update colaborador3.h
    A MI ME TOCO SACAR LA POTENCIA DE LAS MATRICES

--- colaborador3.h
+++ colaborador3.h
@@ -1,8 +1,25 @@
-void potencia(float (*m1)[5], int p, float (*r)[5])
+//Diana Espinal Marin
+//A MI ME TOCO SACAR LA POTENCIA DE LAS MATRICES
+	
+	void potencia(float (*m1)[5], int p, float (*r)[5])
 {
+  cout<<"\nElementos de la matriz1 original : \n";
+for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+	      cout<<m1[i][j]<<" ";
+	}
+cout<<"\n";
+	}
 
 cout<<"Calcular la potencia p de una ";
-
+for(int i=0; i<5; ++i)
+        for(int j=0; j<5; ++j)
+            r[i][j] = 0;
+            
+            for(int i=0; i<5; ++i)
+        for(int j=0; j<5; ++j)
+            for(int z=0; z<5; ++z)
+                r[i][j] += m1[i][z] * m1[z][j];
 
 
 
