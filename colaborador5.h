c5d6836b13f4af0b71133fe6eb6e1f9dbc38dff3
Carlos Mero Alban <86217381+C4rl0sM3r0@users.noreply.github.com>
2021-07-03 21:32:17 -0500

    Create colaborador5.h
    Comencé declarando variables de valor numérico siguiente paso utilice la función for que fue para repetir una o más instrucciones un determinado número de veces.. después utilice una de la variable que es un acum que devuelve el valor de un error luego comenzamos con el procedimientos de las matrices el primero era de sacar la media que está vendría siendo un promedio de las matrices matrices si es una tendencia central  después de esta media sacamos la varianza que es la dispersión que se utiliza para representar la variabilidad de un conjunto de datos respecto de la media aritmética de los mismo luego preguntamos cuál es mayor de las dos matrices y por ende sale la mayor y este es la que nos pide el programa.

--- colaborador5.h
+++ colaborador5.h
@@ -1,9 +1,69 @@
-void mediayvarianza(float (*m1)[5], float (*m2)[])
+void mediayvarianza(float (*m1)[5], float (*m2)[5])
 {
-
-cout<<"Determinar  cual de las 2 matrices tiene la mayor media y varianza";
-
-
-
-
+cout<<endl;
+float acum,acum1,var,cont,med,A,B,C;
+//media aritmética 
+for(int i=0;i<5;i++){
+	
+for(int j=0;j<5;j++){
+	    
+ acum += *(*(m1+j)+i);
+	   
+  cont += 1;
+	
+}
+	
+}
+	
+cout<<endl;
+	
+med=acum/cont;
+	
+cout<<"MATRIZ UNO "<<endl<<endl;
+	
+cout<<"La media es "<<med1<<endl;
+	
+//varianza
+	
+for(int i=0;i<5;i++){
+	
+for(int j=0;j<5;j++){
+	   
+  acum1 += (*(*(m1+j)+i)-med)*(*(*(m1+j)+i)-med);
+}
+	
+}
+	
+cout<<endl;
+	
+var=acum1/cont;
+	
+cout<<"La varianza es "<<var1<<endl;
+	
+//media aritmética 
+for(int i=0;i<5;i++){
+for(int j=0;j<5;j++) 
+A += *(*(m2+j)+i);
+ 
+C += 1;
+}
+}
+cout<<endl;
+med=A/C;
+cout<<"MATRIZ DOS"<<endl<<endl;
+cout<<"La media es "<<med1<<endl;
+//varianza
+for(int i=0;i<5;i++){
+for(int j=0;j<5;j++){
+	 
+B += (*(*(m2+j)+i)-med)*(*(*(m2+j)+i)-med);
+	
+}
+}
+	
+cout<<endl;
+	
+var=B/C;
+	
+cout<<"La varianza es "<<var1<<endl;
 }
