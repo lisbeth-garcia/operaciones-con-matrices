e8d12bb1192d894df165e2c97dd4925bc8b62831
Carlos Mero Alban <86217381+C4rl0sM3r0@users.noreply.github.com>
2021-07-13 17:46:02 -0500

    Update colaborador5.h
    Comencé utilizando un for que esto es un bucle que se utiliza para repetir una o más instrucciones determinado de veces. Después utilizo el int para declaras una variable de tipo entero es decir donde solo puedo almacenar solo números enteros tanto negativos como positivos, en este caso utilizamos este pera las dos matrices  luego comenzamos con las operaciones de las matrices comenzamos sacando las medía aritmética la cual es el valor obtenido de sumar todos los datos y dividir el resultado entro el número total de los datos esto lo hacemos con un for que es donde va a guardar los resultados de las matrices y al igual realizamos la operación con el número de matrices que le ponemos y así la obtendremos luego saque la varianza esta es una medida de dispersión que se utiliza para representar la variabilidad de un conjunto de datos respecto a la media aritmética de los mismo,  así cuál cuñamos la suma de los residuos elevados al cuadrado y divididos entre el total de observaciones.

--- colaborador5.h
+++ colaborador5.h
@@ -1,70 +1,63 @@
-// Carlos Mero Albán 
-void mediayvarianza(float (*m1)[5], float (*m2)[5])
-{
-cout<<endl;
-float acum,acum1,var,cont,med,A,B,C;
-//media aritmética 
-for(int i=0;i<5;i++){
-	
-for(int j=0;j<5;j++){
-	    
- acum += *(*(m1+j)+i);
-	   
-  cont += 1;
-	
-}
-	
-}
-	
-cout<<endl;
-	
-med=acum/cont;
-	
-cout<<"MATRIZ UNO "<<endl<<endl;
-	
-cout<<"La media es "<<med1<<endl;
-	
-//varianza
-	
-for(int i=0;i<5;i++){
-	
-for(int j=0;j<5;j++){
-	   
-  acum1 += (*(*(m1+j)+i)-med)*(*(*(m1+j)+i)-med);
-}
-	
-}
-	
-cout<<endl;
-	
-var=acum1/cont;
-	
-cout<<"La varianza es "<<var1<<endl;
-	
-//media aritmética 
-for(int i=0;i<5;i++){
-for(int j=0;j<5;j++) 
-A += *(*(m2+j)+i);
+void mediayvarianza(float (*m1)[5], float (*m2)[])
+
+//==========================================================================
+//AQUI VOY A CALCULAR Y MOSTRAR LA MEDIA Y VARIANZA DE LAS DOS MATRICES
+  
+  for(int a=0;a<2;a++)
+  {
+     for(int b=0;b<2;b++)
+     {
+      matrizresultado[a][b]=0;
+      matrizresultado[a][b]=matrizresultado[a][b]+matriz1[a][b]*matriz2[a][b];
+      matrizresultado[a][b]=matrizresultado[a][b]/2;
+     }
+  }  
+  
+  for(int a=0;a<2;a++)
+  {
+     for(int b=0;b<2;b++)
+     {
+      matrizresultado1[a][b]=0;
+      matrizresultado1[a][b]=matrizresultado[a][b]+matriz1[a][b]*matriz2[a][b];
+      matrizresultado1[a][b]=matrizresultado[a][b]*2;
+     }
+  }  
+
+
+     cout<<"\nLA MEDIA ARITMETICA ES:\n;
+       
+  for(int a=0;a<2;a++)
+  {
+     for(int b=0;b<2;b++)
+     {
+        cout<<matrizresultado[a][b]<<" ";
+     }
+    
+    cout<<"\n";  
+  }
+     cout<<"\nLA VARIANZA ARITMETICA, QUEDARIA ASI:\n";
  
-C += 1;
+   for(int a=0;a<2;a++)
+  {
+     for(int b=0;b<2;b++)
+     {
+        cout<<matrizresultado1[a][b]<<" ";
+     }
+     cout<<"\n";  
+  }
+     break;
 }
+     if(op<0 || op>5)
+     {
+        cout<<"\nEste numero es valido..\n";
+       system("cls");  
+     }
+   if (op==0)
+   {
+      cout<<"\n Carlos Mero Albán \n";
+       cout<<"TERMINAR: "<<endl;
+   }
 }
-cout<<endl;
-med=A/C;
-cout<<"MATRIZ DOS"<<endl<<endl;
-cout<<"La media es "<<med1<<endl;
-//varianza
-for(int i=0;i<5;i++){
-for(int j=0;j<5;j++){
-	 
-B += (*(*(m2+j)+i)-med)*(*(*(m2+j)+i)-med);
-	
-}
-}
-	
-cout<<endl;
-	
-var=B/C;
-	
-cout<<"La varianza es "<<var1<<endl;
+   while(op!=0);
+   return(0);
 }
