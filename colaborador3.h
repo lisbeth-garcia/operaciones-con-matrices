94ba6b8d4c678843d05680a5adc022f7097dea8b
DianaEspinal06 <86257265+DianaEspinal06@users.noreply.github.com>
2021-07-13 16:24:18 -0500

    Update colaborador3.h
    Para sacar la potencia de las matrices se multiplica cada uno de los valores de las matrices a la que sera sometida al procedimiento para eso se uso el ciclo for para recorrer y después la sentencia de potenciación en donde los valores resultados se almacenarían en la variable matriz resultado.

--- colaborador3.h
+++ colaborador3.h
@@ -1,27 +1,33 @@
-//Diana Espinal Marin 
-
-// POTENCIA DE LAS MATRICES
-	
-	void potencia(float (*m1)[5], int p, float (*r)[5])
+void potencia(float (*m1)[5], int p, float (*r)[5])
 {
-  cout<<"\nElementos de la matriz1 original : \n";
-for(int i=0;i<5;i++){
-	for(int j=0;j<5;j++){
-	      cout<<m1[i][j]<<" ";
-	}
-cout<<"\n";
-	}
 
 cout<<"Calcular la potencia p de una ";
-for(int i=0; i<5; ++i)
-        for(int j=0; j<5; ++j)
-            r[i][j] = 0;
-            
-            for(int i=0; i<5; ++i)
-        for(int j=0; j<5; ++j)
-            for(int z=0; z<5; ++z)
-                r[i][j] += m1[i][z] * m1[z][j];
-
 
 
 }
+//=====================================================
+//COLABORADOR 3
+//DIANA ESPINAL MARIN
+//A MI ME TOCO SACAR LA POTENCIA DE LAS MATRICES
+    //AQUI VAMOS A OBTENER LA POTENCIA DE UNA MATRIZ.
+
+    for (int a=0;a<2;a++)
+    {
+    for (int b=0;b<2;b++)
+         {
+          matrizresultado[a][b]=0;
+          matrizresultado[a][b]=matriz resultado[a][b]+matriz1[a][b]*matriz1[a][b];
+         }
+    }
+    Cout<<"\nPARA LA POTENCIA DE LA MATRIZ 1,ES:\n";
+
+    for(int a=0;a<2;a++)
+    {
+        for(int b=0;b<2;b++)
+        {
+            Cout<<matriz resultado[a][b]<<" ";
+        }
+        Cout<<"\n";
+    }
+
+        break;
